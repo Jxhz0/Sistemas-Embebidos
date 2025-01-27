798ad8fbfbc51e08ab7ce68a448a449accf35dc7
Jxhz0 <josueluna031205@gmail.com>
2025-01-27 17:08:49 -0600

    Update Practica_3.ino

--- Unidad 1/Practica_3.ino
+++ Unidad 1/Practica_3.ino
@@ -1,14 +1,11 @@
 #include <DHT.h>
 #include <LiquidCrystal.h>
-
 // Configuración del DHT
 #define DHTPIN 53       // Pin digital conectado al sensor
 #define DHTTYPE DHT22   // Tipo de sensor DHT22
 DHT dht(DHTPIN, DHTTYPE);
-
 // Configuración del LCD
 LiquidCrystal lcd(8, 9, 4, 5, 6, 7);
-
 // Definimos los valores del botón según el ADC
 #define SELECT_BUTTON 720  // Valor aproximado del botón Select
 #define LEFT_BUTTON 480  // Valor aproximado del botón Left
@@ -20,7 +17,6 @@ LiquidCrystal lcd(8, 9, 4, 5, 6, 7);
 void setup() {
   // Iniciamos el sensor DHT
   dht.begin();
-
   // Iniciamos el LCD
   lcd.begin(16, 2);
   lcd.clear();
@@ -32,7 +28,6 @@ void setup() {
 void loop() {
   // Leer el estado de los botones
   int buttonValue = analogRead(A0);
-
   // Comprobar si se presionó el botón Select
   if (isButtonPressed(buttonValue, SELECT_BUTTON)) {
     mostrarTemperatura();
@@ -41,19 +36,15 @@ void loop() {
   else if (isButtonPressed(buttonValue, LEFT_BUTTON)) {
     E1();
   }
-
   else if (isButtonPressed(buttonValue, RIGHT_BUTTON)) {
     E2();
   }
-
   else if (isButtonPressed(buttonValue, UP_BUTTON)) {
     BD();
   }
-
   else if (isButtonPressed(buttonValue, DOWN_BUTTON)) {
     E3();
   }
-
   delay(200); // Pequeño retardo para evitar lecturas inestables
 }
 
@@ -61,7 +52,6 @@ void loop() {
 void mostrarTemperatura() {
   float t = dht.readTemperature(); // Leer temperatura
   float h = dht.readHumidity();    // Leer humedad
-
   // Validar las lecturas
   if (isnan(t) || isnan(h)) {
     lcd.clear();
@@ -69,14 +59,12 @@ void mostrarTemperatura() {
     lcd.print("Error sensor");
     return;
   }
-
   // Mostrar la temperatura y humedad
   lcd.clear();
   lcd.setCursor(0, 0);
   lcd.print("Temp: ");
   lcd.print(t, 1); // Mostrar temperatura con 1 decimal
   lcd.print(" C");
-
   lcd.setCursor(0, 1);
   lcd.print("Humi: ");
   lcd.print(h, 1); // Mostrar humedad con 1 decimal
